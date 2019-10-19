1a0c57492a43b15f3ac2ecad1ea170cba5f3c9a6
Vasiliy Golban <vgolban@accesssoftek.com>
2019-10-19 20:11:48 +0300

    HW03. Updated initDB and populateDB to init meals.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,20 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+('User', 'user@yandex.ru', 'password'),
+('Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+('ROLE_USER', 100000),
+('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id) VALUES
+('2015-05-30 10:00:00', 'Завтрак', '500', '100001'),
+('2015-05-30 13:00:00', 'Обед', '1000', '100001'),
+('2015-05-30 20:00:00', 'Ужин', '500', '100001'),
+('2015-05-31 10:00:00', 'Завтрак', '1000', '100001'),
+('2015-05-31 13:00:00', 'Обед', '500', '100001'),
+('2015-05-31 20:00:00', 'Ужин', '510', '100001');
